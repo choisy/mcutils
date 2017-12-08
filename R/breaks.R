dacba6db288f48ef8936a4f420e1946a7cacd50b
Marc Choisy <mchoisy@gmail.com>
2017-12-08 15:59:11 +0700

    Adding the breaks function

---
+++ R/breaks.R
@@ -0,0 +1,22 @@
+#' Breaks values
+#'
+#' Returns the breaks values of the output of the \code{base::cut} function
+#'
+#' @param x an output of the \code{base::cut} function.
+#'
+#' @return a numeric vector of break values.
+#'
+#' @export
+#'
+#' @examples
+#' x <- rnorm(10000)
+#' y <- cut(x, breaks = -6:6)
+#' breaks(y)
+#'
+breaks <- function(x) {
+  lev <- levels(x)
+  lev <- gsub("\\(|\\]", "", lev)
+  lev <- gsub(",", " ", lev)
+  lev <- strsplit(paste(lev, collapse = " "), " ")
+  sort(unique(as.numeric(unlist(lev)))) # unlist because strsplit returns a list
+}
