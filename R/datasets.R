9d4ba0fc55dc26fa012abc0a29a933f202c8f6d8
Marc Choisy <marc.choisy@ird.fr>
2018-01-03 18:01:22 +0700

    Append the documentation of the datasets function

--- R/datasets.R
+++ R/datasets.R
@@ -1,12 +1,13 @@
 #' Data Sets
 #'
-#' Shows the datasets of a package
+#' Shows the datasets of a package.
 #'
 #' Returns a character vector containing the names of the datasets in the
-#' package given as the argument
+#' package given as the argument.
 #'
-#' @param pkg the name of a package
+#' @param pkg the name of a package.
 #' @export
+#' @author Marc Choisy.
 datasets <- function(pkg) {
   data(package = pkg)$results[, "Item"]
 }
