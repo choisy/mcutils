890e297f83cddbab76dd825ea7af79661148ae77
Marc Choisy <marc.choisy@ird.fr>
2018-11-15 15:45:03 +0700

    Adding the function print_units in the file utils

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+print_units <- function(x) {
+  un <- units(a$elevation)
+  numerator <- un$numerator
+  denominator <- un$denominator
+  if (length(numerator) > 0) {
+    if (length(denominator) > 0) un <- paste0(numerator, "/", denominator)
+    else un <- numerator
+  } else {
+    if (length(denominator) > 0) un <- paste0("/", denominator)
+    else un <- ""
+  }
+  paste0(x, " [", un, "]")
+}
